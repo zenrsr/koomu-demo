da8b07312daededf1f575b30e182bc0e51af64d7
zenrsr <ragasandeepreddy25@gmail.com>
2024-09-15 16:06:00 +0530

    feat: Add (17/20) phase of development

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import { ThemeProvider } from "@/components/theme"
+import { ReactQueryProvider } from "@/react-query/provider"
+import { ReduxProvider } from "@/redux/provider"
+import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from "next"
+import { Plus_Jakarta_Sans } from "next/font/google"
+import { Toaster } from "sonner"
+import "./global.css"
+
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Kommu",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${jakarta.className} bg-black`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            <ReduxProvider>
+              <ReactQueryProvider>{children}</ReactQueryProvider>
+            </ReduxProvider>
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
